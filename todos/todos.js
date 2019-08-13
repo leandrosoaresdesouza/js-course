f7d2a3df550963821af6678ad3c812a8e1d2d010
Leandro Soares <lsoares@orio.tech>
2019-08-13 17:55:02 -0300

    Initial commit

---
+++ todos/todos.js
@@ -0,0 +1,51 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = JSON.parse(localStorage.getItem('listTodos'))  || [];
+
+function renderTodos() {
+  listElement.innerHTML = '';
+
+  for (todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+
+    var linkElement = document.createElement('a');
+
+    linkElement.setAttribute('href', '#');
+
+    var position = todos.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo(' + position + ')');
+
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
+
+    todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
+
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
+
+function addTodo() {
+  var todoText = inputElement.value;
+
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+  saveToStorage();
+}
+
+function deleteTodo(pos) {
+  todos.splice(pos, 1);
+  renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('listTodos', JSON.stringify(todos));
+}
