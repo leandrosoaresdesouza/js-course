10c1c3f560fd64a13404e7e9b02ee63e4b485c7f
Leandro Soares <lsoares@orio.tech>
2019-08-14 07:27:38 -0300

    Studying webpack

---
+++ es6/webpack/divisao.js
@@ -0,0 +1,3 @@
+export default function divisao(a, b) {
+  return a / b;
+}
---
+++ es6/webpack/src/main.js
@@ -0,0 +1,11 @@
+import { soma as somaFunction, sub } from '../funcoes';
+import divisaoFunction from '../divisao';
+
+console.log(somaFunction(1, 2));
+
+console.log(sub(10, 1));
+
+console.log(divisaoFunction(10,2));
+
+
+alert('testeeeee');
